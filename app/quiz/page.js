d9915725d8768e90b888e11d19518ad4d0e91dc7
lcvargas <Luis.Mercon@criticalsoftware.com>
2023-11-23 09:51:49 +0000

    refactoring

--- app/quiz/page.js
+++ app/quiz/page.js
@@ -1,124 +1,92 @@
-'use client'
-import React, { useState } from "react";
-import { quiz } from "../data";
+'use client';
+import React, { useState } from 'react';
+import { quiz } from '../data';
 import confetti from 'canvas-confetti';
 
 const page = () => {
-
-
     const [activeQuestion, setActiveQuestion] = useState(0);
-    const [selectedAnswer, setSelectedAnswer] = useState('');
-    const [checked, setChecked] = useState(false);
+    const [selectedAnswer, setSelectedAnswer] = useState(false);
     const [selectedAnswerIndex, setSelectedAnswerIndex] = useState(null);
-    const [showResult, setShowResult] = useState(false)
-    const [result, setResult] = useState({
-        score: 0,
-        correctAnswers: 0,
-        wrongAnswers: 0
-    });
+    const [showResult, setShowResult] = useState(false);
+    const [result, setResult] = useState({ score: 0, correctAnswers: 0, wrongAnswers: 0 });
 
-    const { questions } = quiz;
-    const { question, answers, correctAnswer } = questions[activeQuestion]
+    const { question, answers, correctAnswer } = quiz.questions[activeQuestion];
 
-    const onAnswerSelected = (answer, idx) => {
-        setChecked(true)
-        setSelectedAnswerIndex(idx)
-        if (answer === correctAnswer) {
-            setSelectedAnswer(true)
-            console.log('true')
-            // Trigger confetti
-            triggerConfetti();
-        } else {
-            setSelectedAnswer(false)
-            console.log('false')
+    const handleAnswerSelection = (answer, index) => {
+        setSelectedAnswerIndex(index);
+        const isCorrect = answer === correctAnswer;
+        setSelectedAnswer(isCorrect);
+
+        if (isCorrect) {
+            confetti({ particleCount: 200, spread: 170, origin: { y: 0.6 } });
         }
-    }
 
-    const triggerConfetti = () => {
-        confetti({
-            particleCount: 200,
-            spread: 170,
-            origin: { y: 0.6 }
-        });
+        updateResult(isCorrect);
     };
 
-    const nextQuestion = () => {
-        setSelectedAnswerIndex(null)
-        setResult((prev) =>
-            selectedAnswer ?
-                {
-                    ...prev,
-                    score: prev.score + 1,
-                    correctAnswers: prev.correctAnswers + 1,
-                }
-                :
-                {
-                    ...prev,
-                    wrongAnswers: prev.wrongAnswers + 1,
-                }
-        )
-        if (activeQuestion !== questions.length - 1) {
-            setActiveQuestion((prev) => prev + 1)
+    const updateResult = (isCorrect) => {
+        setResult((prev) => ({
+            ...prev,
+            score: isCorrect ? prev.score + 1 : prev.score,
+            correctAnswers: isCorrect ? prev.correctAnswers + 1 : prev.correctAnswers,
+            wrongAnswers: isCorrect ? prev.wrongAnswers : prev.wrongAnswers + 1
+        }));
+    };
+
+    const goToNextQuestion = () => {
+        const isLastQuestion = activeQuestion === quiz.questions.length - 1;
+        if (isLastQuestion) {
+            setShowResult(true);
+            setActiveQuestion(0);
         } else {
-            setActiveQuestion(0)
-            setShowResult(true)
+            setActiveQuestion((prev) => prev + 1);
         }
-        setChecked(false)
-    }
+        setSelectedAnswer(false);
+        setSelectedAnswerIndex(null);
+    };
 
     return (
         <div className="container">
             <h1>Quiz Page</h1>
+            {!showResult ? <Quiz activeQuestion={activeQuestion} question={question} answers={answers} selectedAnswer={selectedAnswer} selectedAnswerIndex={selectedAnswerIndex} handleAnswerSelection={handleAnswerSelection} goToNextQuestion={goToNextQuestion} /> : <Results result={result} />}
+        </div>
+    );
+};
 
-            <div>
-                <h2>Question: {activeQuestion + 1}
-                    <span>/{questions.length}</span>
-                </h2>
-            </div>
+const Quiz = ({ activeQuestion, question, answers, selectedAnswer, selectedAnswerIndex, handleAnswerSelection, goToNextQuestion }) => (
+    <div>
+        <h2>
+            Question: {activeQuestion + 1}
+            <span>/{quiz.questions.length}</span>
+        </h2>
+        <div className="quiz-container">
+            <h3>{question}</h3>
+            {answers.map((answer, idx) => (
+                <li key={idx}
+                    onClick={() => handleAnswerSelection(answer, idx)}
+                    className={selectedAnswerIndex === idx ? 'li-selected' : 'li-hover'}
+                    style={{
+                        backgroundColor: selectedAnswer && answer === answers[selectedAnswerIndex] ? 'green' : 'transparent',
+                        color: selectedAnswer && answer === answers[selectedAnswerIndex] ? 'white' : '#000105'
+                    }}>
+                    {answer}
+                </li>
+            ))}
+            {selectedAnswer && (
+                <button onClick={goToNextQuestion} className="btn">
+                    {activeQuestion === quiz.questions.length - 1 ? 'Finish' : 'Next'}
+                </button>
+            )}
+        </div>
+    </div>
+);
 
-            <div>
-                {!showResult ?
-                    (
-                        <div className="quiz-container" >
-                            <h3>{questions[activeQuestion].question}</h3>
-                            {answers.map((answer, idx) => (
-                                <li key={idx}
-                                    onClick={() => onAnswerSelected(answer, idx)}
-                                    className={selectedAnswerIndex === idx ? "li-selected" : "li-hover"}
-                                    style={{
-                                        backgroundColor: selectedAnswer && answer === correctAnswer && selectedAnswerIndex === idx ? 'green' : 'transparent',
-                                        color: selectedAnswer && answer === correctAnswer && selectedAnswerIndex === idx ? 'white' : '#000105'
-                                    }}>
-                                    <span> {answer} </span>
-                                </li>
-                            ))}
-                            {checked && selectedAnswer &&
-                                <button onClick={nextQuestion} className="btn">
-                                    {activeQuestion === questions.length - 1 ? "Finish" : "Next"}
-                                </button>
-                            }
-                        </div>
-                    )
-                    :
-                    (
-                        <div className="quiz-container">
-                            <h3>Welcome to the finish line!</h3>
+const Results = ({ result }) => (
+    <div className="quiz-container">
+        <h3>Welcome to the finish line!</h3>
+        <img src="/finish_line2.png" alt="Finish Line" className="responsive-image" />
+        <button onClick={() => window.location.reload()}>Restart</button>
+    </div>
+);
 
-                            <img src="\finish_line2.png" alt="Finish Line" className="responsive-image" />
-
-                            <button onClick={() => window.location.reload()} >Restart</button>
-
-
-                        </div>
-                    )
-                }
-
-            </div>
-
-
-
-        </div >
-    )
-}
-
-export default page
+export default page;
